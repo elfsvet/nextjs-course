93650815b2a9c69788c46b950dec9087f584452e
elfsvet <stepanmatysik@gmail.com>
2022-11-17 23:45:06 -0500

    added new way of managing styling

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     // to use two classes in the className with module
-    <div className={`${styles.container} ${styles.main}`}>
+    // all three are valid options
+
+    <div className={styles.container + ' ' + styles.main}>
+      {/* <div className={[styles.container, styles.main].join(' ')}>  */}
+      {/* <div className={`${styles.container} ${styles.main}`}> */}
       <h1>Hello Next World!</h1>
     </div>
   );
